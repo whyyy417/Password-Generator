c32d985a56bc02fa4e38c96118ff438c63c75a97
whyyy417 <joselynramirez92@gmail.com>
2022-09-23 10:18:48 -0500

    Formatted Copy Button & added hover style

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+let characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
 "/"];
 
 const generateBtn = document.querySelector("#generate-btn");
-const passOne = document.querySelector("#Password-one");
-const passTwo= document.querySelector("#Password-two");
+let passOne = document.querySelector("#Password-one");
+let passTwo= document.querySelector("#Password-two");
 
 
 let passLength = 14;
@@ -29,6 +29,17 @@ function generatePassTwo(){
         passTwo += characters[randomPassTwo]
     };
     return passTwo
-}
+};
 
+const copyBtnOne = document.querySelector("#copy-one");
+const copyBtnTwo = document.querySelector("#copy-two")
 
+copyBtnOne.addEventListener("click", function(){
+    textOne = passOne.textContent;
+    navigator.clipboard.writeText(textOne);
+})
+
+copyBtnTwo.addEventListener("click", function(){
+    textTwo = passTwo.textContent;
+    navigator.clipboard.writeText(textTwo);
+})
